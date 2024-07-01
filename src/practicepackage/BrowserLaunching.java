bd0fd78cc6122db1d9f94e8b8d5d43b6883da104
kalpeshsc <kalpeshsc@teamlocus.ent>
2024-07-01 17:02:35 +0530

    new change

---
+++ src/practicepackage/BrowserLaunching.java
@@ -0,0 +1,23 @@
+package practicepackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunching {
+
+	public static void main(String[] args) {
+
+		// browser setup after selenium 4 implementataion
+		ChromeOptions chrome = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(chrome);
+
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		System.out.println("Browser Launching successfully");
+	}
+
+}
